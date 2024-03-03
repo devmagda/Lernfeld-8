8a8fe0459fb1c5f85700f8a42188e2955c0e45ac
devmagda <113769198+devmagda@users.noreply.github.com>
2024-03-04 00:30:57 +0100

    .

---
+++ docker/jenkins_data/.bash_history
@@ -0,0 +1,10 @@
+ping nexus
+ls
+ping
+apt install netutils
+sudo apt install netutils
+su 
+exit
+ls
+locate
+exit
---
+++ docker/jenkins_data/jobs/Project-Pipeline/builds/9/jacoco/sources/edu/p/eight/Main.java
@@ -0,0 +1,11 @@
+package edu.p.eight;
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println(StringUtils.add("Hello, ", null));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
